f4bbe5484920203460f23c41ae2928ca726c4fbf
Wessl <lovewessman@gmail.com>
2021-03-13 20:48:00 +0100

    Add initial project files

---
+++ MayaPyramidTest/Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody bd;
+
+    public float speed;
+    void Start()
+    {
+        bd = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bd.AddForce(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed, 0, ForceMode.VelocityChange);
+
+    }
+}
