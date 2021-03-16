ac8fe209ee621ac897c766a2cd388c8ddcef05e3
Wessl <lovewessman@gmail.com>
2021-03-16 20:50:09 +0100

    Added/experimenting with animations from Mixamo
    Very cool Mixamo, thank you
    
    Basically added a default character that swaps between animations. Press T or T and left shift and swap between idle, walking and running. Looking to expand on this and use it for NPCs.

---
+++ MayaPyramidTest/Assets/AnimationStateController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+    private Animator _animator;
+    private int isWalkingHash;
+    private int isRunningHash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isRunning = _animator.GetBool(isRunningHash);
+        bool isWalking = _animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("t");
+        bool runPressed = Input.GetKey("left shift");
+        if (!isWalking && forwardPressed)
+        {
+            _animator.SetBool(isWalkingHash, true);
+        }
+        if (isWalking && !forwardPressed)
+        {
+            _animator.SetBool(isWalkingHash, false);
+        }
+
+        if (!isRunning && (forwardPressed && runPressed))
+        {
+            _animator.SetBool(isRunningHash, true);
+        }
+
+        if (isRunning && (!forwardPressed || !runPressed))
+        {
+            _animator.SetBool(isRunningHash, false);
+        }
+    }
+}
