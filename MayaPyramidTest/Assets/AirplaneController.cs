0cf48379e8aa01c66258584080c991fe7a378a57
Wessl <lovewessman@gmail.com>
2021-03-23 23:50:23 +0100

    Slightly smoother movement with Mathf.Lerp

--- MayaPyramidTest/Assets/AirplaneController.cs
+++ MayaPyramidTest/Assets/AirplaneController.cs
@@ -6,6 +6,7 @@ public class AirplaneController : MonoBehaviour
 {
     public float forwardSpeed = 25f, strafeSpeed = 7.5f, hoverSpeed = 5f;
     private float _activeForwardSpeed, _activeStrafeSpeed, _activeHoverSpeed;
+    private float _forwardAcceleration = 2.5f, _strafeAcceleration = 2f, _hoverAcceleration = 2f; 
     void Start()
     {
         
@@ -13,9 +14,9 @@ public class AirplaneController : MonoBehaviour
 
     void Update()
     {
-        _activeForwardSpeed = Input.GetAxisRaw("Vertical") * forwardSpeed;
-        _activeStrafeSpeed = Input.GetAxisRaw("Horizontal") * strafeSpeed;
-        _activeHoverSpeed = Input.GetAxisRaw("Hover") * hoverSpeed;
+        _activeForwardSpeed = Mathf.Lerp(_activeForwardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, _forwardAcceleration * Time.deltaTime);
+        _activeStrafeSpeed = Mathf.Lerp(_activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, _strafeAcceleration * Time.deltaTime);
+        _activeHoverSpeed = Mathf.Lerp(_activeHoverSpeed, Input.GetAxisRaw("Hover") * hoverSpeed, _hoverAcceleration * Time.deltaTime);
         transform.position += _activeForwardSpeed * Time.deltaTime * transform.forward;
         transform.position += _activeStrafeSpeed * Time.deltaTime * transform.right;
         transform.position += _activeHoverSpeed * Time.deltaTime * transform.up;
